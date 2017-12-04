514a0f335f7a01476f4f5420c5ad2ae1fd0a3f36
Dennis Neve <dennis.neve@hotmail.com>
2017-12-04 16:25:28 +0100

    Adds drawer script changes inventory

---
+++ Bibber Doctor/Assets/Scripts/VANASDrawers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VANASDrawers : MonoBehaviour {
+
+    public GameObject[] drawers;
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject drawer in drawers)
+        {
+            drawer.GetComponent<VRTK.VRTK_InteractableObject>().isGrabbable = false;
+            drawer.transform.GetChild(0).gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UnlockDrawer(int drawerNmbr)
+    {
+        drawers[drawerNmbr].transform.GetChild(0).gameObject.SetActive(true);
+        drawers[drawerNmbr].GetComponent<VRTK.VRTK_InteractableObject>().isGrabbable = true;
+    }
+}
