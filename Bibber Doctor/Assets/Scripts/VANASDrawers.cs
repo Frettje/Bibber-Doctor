994f5d3fde8c979a91802ba9a074805cc32e7bc2
Dennis Neve <dennis.neve@hotmail.com>
2017-12-06 13:16:32 +0100

    Adds drawers to VANAS model, scanner and drawers scripts

--- Bibber Doctor/Assets/Scripts/VANASDrawers.cs
+++ Bibber Doctor/Assets/Scripts/VANASDrawers.cs
@@ -1,28 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class VANASDrawers : MonoBehaviour {
 
     public GameObject[] drawers;
+    public GameObject[] contents;
 
-	// Use this for initialization
-	void Start () {
+    public float openingMin = 0.07F;
+    public float openingMax = 0.26f;
+    public float openingIncrement;
+
+    bool unlock = true;
+
+    void Start () {
 		foreach (GameObject drawer in drawers)
         {
-            drawer.GetComponent<VRTK.VRTK_InteractableObject>().isGrabbable = false;
-            drawer.transform.GetChild(0).gameObject.SetActive(false);
+            // Turns off the light of each drawer and locks them in closed position.
+
+            drawer.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            drawer.GetComponent<VRTK_InteractableObject>().enabled = false;
+            drawer.transform.GetChild(1).gameObject.SetActive(false);
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    public void UnlockDrawer(int drawerNmbr)
+    private void Update()
     {
-        drawers[drawerNmbr].transform.GetChild(0).gameObject.SetActive(true);
-        drawers[drawerNmbr].GetComponent<VRTK.VRTK_InteractableObject>().isGrabbable = true;
+        int drawerNmbr = 0;     // For testing
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleLock(drawerNmbr, unlock);
+            unlock = !unlock;
+        }
+    }
+
+    public void ToggleLock(int drawerNmbr, bool unlock)
+    {
+        // Toggles between drawers locked and unlocked, light on and off
+
+        if (unlock)
+        {
+            drawers[drawerNmbr].GetComponent<Rigidbody>().constraints &= ~RigidbodyConstraints.FreezePositionZ;
+        }
+        else
+        {
+            drawers[drawerNmbr].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+
+        drawers[drawerNmbr].transform.GetChild(1).gameObject.SetActive(unlock);
+        drawers[drawerNmbr].GetComponent<VRTK_InteractableObject>().enabled = unlock;
+        contents[drawerNmbr].SetActive(unlock);
+
+        // Calculate opening by number of items in contents
+
+        //drawers[drawerNmbr].GetComponent<ConfigurableJoint>().linearLimit.limit = openingMin;
     }
 }
---
+++ Bibber Doctor/Assets/Scripts/VANASScanner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VANASScanner : MonoBehaviour {
+
+    public GameObject VANASInterface;
+    public Light scanFeedback;
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "Badge")
+        {
+            StartCoroutine(OnScan());
+            // Send ID OK
+        }
+    }
+
+    IEnumerator OnScan ()
+    {
+        // Play sound
+        scanFeedback.color = Color.green;
+        yield return new WaitForSeconds(1);
+        scanFeedback.color = new Color (38, 225, 240);
+    }
+}
