3459ff493673d50e7ccdfe4c958f1d97c0d3b35c
BouwensBart <bart.bouwens@hotmail.com>
2017-12-09 17:29:11 +0100

    Adds Interface
    First version of the VANAS Interface with search function and patient information (ID, Name, prescription)

---
+++ Bibber Doctor/Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceManager : MonoBehaviour {
+
+    public GameObject keyboard;
+    public Canvas searchInterface;
+    public Canvas patientID;
+    public Patient[] patientList;
+    public Patient patientToTreat;
+    public Text input;
+    public Text id;
+    public Text fullName;
+    public Text prescription;
+    public string inputStr;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        inputStr = input.text;
+	}
+
+    public void Searchpatient(string patientName)
+    {
+        patientName = inputStr;
+
+        for (int i = 0; i < patientList.Length; i++)
+        {
+            if(patientList[i].name == patientName || patientList[i].ID == int.Parse(patientName))
+            {
+                id.text = patientList[i].ID.ToString();
+                fullName.text = patientList[i].name;
+                prescription.text = patientList[i].prescription;
+            }
+        }
+
+        searchInterface.enabled = false;
+        keyboard.SetActive(false);
+        patientID.enabled = true;
+
+    }
+
+	public void Back(string patienName)
+	{
+        patientID.enabled = false;
+        searchInterface.enabled = true;
+        keyboard.SetActive(true);
+	}
+
+}
---
+++ Bibber Doctor/Assets/Scripts/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Genders {male, female};
+
+public class Patient : MonoBehaviour {
+
+    public int ID;
+    public string patientName;
+    public int patientAge;
+    public Genders gender;
+    public float weigth;
+    public bool pregnant = false;
+    public List<string> complications = new List<string>();
+    public List<string> previousDosages = new List<string>();
+    public string prescription;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
