13588d09100dc5664ac24923158a668244faafc8
BouwensBart <bart.bouwens@hotmail.com>
2018-01-24 19:47:39 +0100

    Update Syringe & Patients
    Updated version of the syringe prefab

---
+++ Bibber Doctor/Assets/Scripts/Press.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Press : MonoBehaviour {
+
+    public float maxClamp = 0.047f;
+    public float minClamp = 0.01f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.gameObject.transform.position = new Vector3(0, Mathf.Clamp(this.gameObject.transform.position.y, minClamp, maxClamp), 0);
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Fluid")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
--- Bibber Doctor/Assets/Scripts/Syringe.cs
+++ Bibber Doctor/Assets/Scripts/Syringe.cs
@@ -9,10 +9,15 @@ public class Syringe : MonoBehaviour {
     public Vector3 newNeedlePos;
     public Vector3 needlePos;
     public bool hasNeedle = false;
-    public Collider[] detectedNeedles = new Collider[1];
-    public GameObject[] needles = new GameObject[27];
     public SphereCollider radius;
+
+    //Needle Parent
     public GameObject needleCol;
+
+    //Fluid
+    public GameObject fluid;
+    public GameObject press;
+    public float pressYPos;
 
     protected float dist = 5.0f;
 
@@ -22,67 +27,19 @@ public class Syringe : MonoBehaviour {
 	void Start ()
     {
         newNeedlePos = needlePlace.transform.position;
-        radius = needlePlace.GetComponent<SphereCollider>();
+        radius = GetComponent<SphereCollider>();
+        pressYPos = press.transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //needlePos = needle.transform.position;
-        //GetNeedle();
-
-        //CheckNeedleProximity();
+        AddFluid();
 
         Debug.Log(hasNeedle);
         Debug.Log(Vector3.Distance(needlePos, newNeedlePos));
         //Debug.Log(needle);
 	}
-
-    //public void GetNeedle()
-    //{
-    //    detectedNeedles = Physics.OverlapSphere(newNeedlePos, 0.1f);
-    //    Debug.Log(detectedNeedles[0].gameObject.name);
-
-    //    foreach(Collider col in detectedNeedles)
-    //    {
-    //        if (col.gameObject.tag == "needle" && hasNeedle == false)
-    //        {
-    //            needle = detectedNeedles[0].gameObject;
-    //            hasNeedle = true;
-    //            if (Vector3.Distance(needlePos, newNeedlePos) > 0.1f)
-    //            {
-    //                detectedNeedles[0] = null;
-    //                hasNeedle = false;
-    //            }
-    //        }
-    //    }
-
-    //    needle.transform.position = newNeedlePos;
-    //    needle.transform.parent = this.transform;
-
-    //}
-
-    public void CheckNeedleProximity()
-    {
-        for (int i = 0; i < needles.Length; i++)
-        {
-            if(Vector3.Distance(needles[i].transform.position, newNeedlePos) < dist && !hasNeedle)
-            {
-                hasNeedle = true;
-                needle = needles[i];
-                needlePos = newNeedlePos;
-                needle.transform.parent = this.transform;
-
-            }
-            Debug.Log(Vector3.Distance(needles[i].transform.position, newNeedlePos));
-        }
-
-        if(Vector3.Distance(needlePos, newNeedlePos) > dist)
-        {
-            hasNeedle = false;
-            needle = null;
-        }
-    }
 
     public void OnTriggerEnter(Collider other)
     {
@@ -105,5 +62,14 @@ public class Syringe : MonoBehaviour {
             needle.transform.parent = needleCol.transform;
             hasNeedle = false;
 		}
+    }
+
+    public void AddFluid()
+    {
+        if (press.transform.position.y > pressYPos && pressYPos <= 0.047f)
+        {
+            Instantiate(fluid);
+            pressYPos = press.transform.position.y;
+        }
     }
 }
